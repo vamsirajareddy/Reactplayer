8a413599d9e3b058af9bc6cab53060fdf3d57ca1
Vamsi Rajareddy <vamsin135@gmail.com>
2017-04-05 21:33:34 -0400

    Made changes to the react player in the search bar

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyDt1k6aqfE2cSF3tdnjfDzqLAn2gQCJp-4';
+
 //create a new component. Thics component should produce some HTML
 const App = () => {
-  return <div>hello</div>;
+  return(
+     <div>
+  <SearchBar />
+  </div>
+);
 }
 // This component generated HTML and put it on the page(in the DOM)
 ReactDom.render(<App />, document.querySelector('.container'));
